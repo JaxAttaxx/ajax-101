dcd129dfa65ac2fcdfa9358c30875721124c7c4a
Jackson Godwin <jaxjunkmail80@gmail.com>
2020-09-16 00:45:00 -0400

    Wed pre-work

---
+++ ajax101sm.js
@@ -0,0 +1,15 @@
+const request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        console.log(this);
+    }
+};
+request.open("GET", "./sample.txt");
+request.send();
+
+//I believe same above as below
+
+fetch("./sample.txt").then((response) => {
+    console.log(response);
+});
+
